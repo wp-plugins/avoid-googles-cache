3f628502f83473cd97bd875bdc5b0f356f522049
kezze <kezze@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-05-23 12:37:14 +0000

    Version check and banner for Wordpress.org
    git-svn-id: https://plugins.svn.wordpress.org/avoid-googles-cache/tags/1.2@548026 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ trunk/avoid-googles-cache.php
@@ -0,0 +1,33 @@
+<?php
+/*
+Plugin Name: Avoid Google's Cache
+Plugin URI: http://wordpress.org/extend/plugins/avoid-googles-cache/
+Description: Avoid getting your blog cached by e.g. Google and Internet Archive.
+Author: Kristian Risager Larsen
+Version: 1.2
+Author URI: http://kezze.dk
+*/
+
+add_action("wp_head", "avoid_cache_google");
+
+function avoid_cache_google()
+{
+	echo "\n\n<!-- Google and other search engines should not cache this page. Plugin by Kristian Risager Larsen, http://kezze.dk . Download the plugin at http://wordpress.org/extend/plugins/avoid-googles-cache/ -->\n";
+    echo "<meta name=\"robots\" content=\"noarchive\" />\n\n";
+}
+
+// http://www.archive.org/about/exclude.php
+
+add_action("do_robotstxt","avoid_cache_internet_archive");
+
+function avoid_cache_internet_archive()
+{
+    echo "\n\n";
+    echo "# The Internet Archive (at http://web.archive.org/ ) should not cache this page.\n";
+	echo "# Plugin by Kristian Risager Larsen, http://kezze.dk . Download the plugin at http://wordpress.org/extend/plugins/avoid-googles-cache/\n";
+    echo "User-agent: ia_archiver\n";
+    echo "Disallow: /";
+    echo "\n\n";
+}
+
+?>
---
+++ trunk/readme.txt
@@ -0,0 +1,32 @@
+=== Avoid Google's Cache ===
+Contributors: kezze
+Donate link: https://www.paypal.com/xclick/business=paypal%40kezze.dk&item_name=Donation&no_note=1&tax=0&currency_code=EUR
+Tags: google, cache, google cache, google's cache, internet archive
+Requires at least: 2.0
+Tested up to: 3.3
+Stable tag: 1.2
+
+Avoid getting your blog cached by e.g. Google and Internet Archive.
+
+== Description ==
+Avoid getting your blog cached by e.g. Google and Internet Archive.
+
+** Credits **
+The banner is a composition of two photos by '[Skimaniac](http://www.flickr.com/photos/skimaniac/100495205/)' and [Patrick Brosset](http://www.flickr.com/photos/gnackgnackgnack/3942331668/).
+
+== Installation ==
+1. Copy the plugin to `/wp-content/plugins/`
+1. Activate the plugin.
+
+== Changelog ==
+= 1.2 =
+Added: Exclusion from Internet Archive ( http://web.archive.org/ )
+
+= 1.1 =
+Fixed: Donate-link and Tags in readme.txt 
+
+= 1.0 =
+Nothing really...
+
+= 0.9 =
+Initial release
